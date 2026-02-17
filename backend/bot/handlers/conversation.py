de36a03ad9d58dd38ce0f7bd114e9ff4f54a6796
Bigbear2006 <morozm982@gmail.com>
2026-02-17 16:41:55 +0300

    fix(conversation)

--- backend/bot/handlers/conversation.py
+++ backend/bot/handlers/conversation.py
@@ -4,6 +4,7 @@ from aiogram import F, Router, flags
 from aiogram.types import Message
 from django.core.exceptions import ObjectDoesNotExist
 
+from bot.loader import logger
 from bot.services.match import get_soulmate
 
 router = Router()
@@ -18,8 +19,10 @@ async def forward_message_handler(msg: Message, thread_id: int) -> None:
         await msg.answer('Мэтч не найден')
         return
 
-    with contextlib.suppress(TypeError):
+    try:
         await msg.send_copy(
             soulmate.user.id,
             message_thread_id=soulmate.thread_id,
         )
+    except Exception as e:
+        logger.exception(e)
